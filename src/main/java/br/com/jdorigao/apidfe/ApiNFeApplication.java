f1f487f3710293f0c012f818c66f38005d38f9fd
Juliano Dorigão <jdorigao@gmail.com>
2022-03-25 09:11:46 -0400

    Spring Boot configurado

---
+++ src/main/java/br/com/jdorigao/apidfe/ApiNFeApplication.java
@@ -0,0 +1,13 @@
+package br.com.jdorigao.apidfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNFeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiNFeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jdorigao/apidfe/ApiNFeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jdorigao.apidfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNFeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
